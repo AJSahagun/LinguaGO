9ac48494f310289a3a5ac8c63d01c93aa19577b2
Cloud Rain <88638495+Cl0udRain@users.noreply.github.com>
2022-05-24 21:31:57 +0800

    Alpha 0.1.1

--- gui.py
+++ gui.py
@@ -1,33 +1,38 @@
+# NOTE:
+# linguago.py and icon images must be on the same folder as gui.py
+# Libraries to be installed:
+#   - ttkwidgets
+from linguago import LinguaGo
+from threading import Thread
 from tkinter import *
 from tkinter import ttk, messagebox
-import speech_recognition
 from ttkwidgets.autocomplete import AutocompleteCombobox
-from linguago import LinguaGo
+from idlelib.tooltip import Hovertip
+import requests.exceptions
+import speech_recognition
 
-root = Tk()
-root.title('LinguaGO')
-root.configure(background='#1e1e24')
-icon = PhotoImage(file=r"L.png")
-# a raw string of the file path to be recognized as icon
-root.iconphoto(False, icon)
-root.geometry("824x350")
+# Object for LinguaGo
 lg = LinguaGo()
 
 
 # FUNCTIONS FOR BUTTONS AND FUNCTIONALITY
 def translate():
-    output_box.delete(1.0, END)
-    trans_lang = combo_trans.get()
-    source_lang = combo_1.get()
-    text_to_translate = input_box.get(1.0, END)
-    translation = lg.translate(text_to_translate, source_lang, trans_lang)
-    output_box.insert(1.0, translation)
-    detect()
-    if trans_lang in lg.character:
-        label2.set(f"Romanization: {lg.romanize(translation, trans_lang)}")
+    try:
+        output_box.delete(1.0, END)
+        trans_lang = combo_trans.get()
+        source_lang = combo_1.get()
+        text_to_translate = input_box.get(1.0, END)
+        translation = lg.translate(text_to_translate, source_lang, trans_lang)
+        output_box.insert(1.0, translation)
+        detect()
+        if trans_lang in lg.character:
+            label2.set(f"Romanization: {lg.romanize(translation, trans_lang)}")
+    except requests.exceptions.ConnectionError:
+        messagebox.showerror('Error', "No internet detected.")
 
 
 def detect(show=True):
+    label1.set('')
     if combo_1.get() == 'Auto':
         detected = lg.detect(input_box.get(1.0, 'end-1c'))
         if show:
@@ -41,14 +46,20 @@ def text_to_speech_input():
     if input_box.get(1.0, END) == '\n':
         messagebox.showerror('Error', "No Text Found.")
     else:
+        button_tts1['state'] = DISABLED
+        detect()
         lg.speak(input_box.get(1.0, 'end-1c'), combo_1.get())
+        button_tts1['state'] = NORMAL
 
 
 def text_to_speech_output():
     if output_box.get(1.0, END) == '\n':
         messagebox.showerror('Error', "No Text Found.")
     else:
+        button_tts1['state'] = DISABLED
+        detect()
         lg.speak(output_box.get(1.0, END), combo_trans.get())
+        button_tts1['state'] = NORMAL
 
 
 def spellcheck_input():
@@ -63,6 +74,7 @@ def spellcheck_input():
 
 
 def speech_to_text():
+    button_stt['state'] = DISABLED
     try:
         text = lg.recognize()
     except speech_recognition.UnknownValueError:
@@ -71,6 +83,7 @@ def speech_to_text():
         messagebox.showerror('Error', "Couldn't request result from Google.")
     else:
         input_box.insert(1.0, text)
+        button_stt['state'] = NORMAL
 
 
 def clear():
@@ -94,8 +107,18 @@ def switcher():
 
 
 # GUI
+
+root = Tk()
+root.title('LinguaGO')
+root.configure(background='#1e1e24')
+icon = PhotoImage(file=r"LinguaGo-Logo_Circle.png")
+# A raw string of the file path to be recognized as icon
+root.iconphoto(False, icon)
+root.geometry("824x350")
 st = ttk.Style()
 st.theme_use('xpnative')
+
+
 # TEXT BOXES
 # INPUT TEXT BOX
 input_box = Text(root,
@@ -114,6 +137,7 @@ output_box.grid(row=1, column=4, padx=10, pady=10)
 # DISABLE INPUT IN OUTPUT BOX
 output_box.bind("<Button-1>", lambda e: 'break')
 output_box.bind("<Key>", lambda e: 'break')
+
 
 # LABELS
 label1 = StringVar()
@@ -135,6 +159,7 @@ Label(root,
       font='lato',
       bg='#1e1e24',).grid(row=2, column=4, columnspan=2)
 
+
 # BUTTONS
 # TRANSLATE BUTTON
 trans_butt = Button(root,
@@ -142,7 +167,7 @@ trans_butt = Button(root,
                     bg="#e0e0e0",
                     fg="#46474f",
                     command=translate)
-root.bind('<Return>', lambda e: translate())
+trans_butt_tt = Hovertip(trans_butt, "Translate")
 trans_butt.grid(row=1, column=3, padx=10, pady=10)
 
 # TTS BUTTON 1
@@ -151,7 +176,8 @@ button_tts1 = Button(root,
                      image=tts_photo,
                      bg="#e0e0e0",
                      fg="#46474f",
-                     command=text_to_speech_input)
+                     command=lambda: Thread(target=text_to_speech_input).start())
+button_tts1_tt = Hovertip(button_tts1, "Text-To-Speech")
 button_tts1.grid(row=4, columnspan=1, padx=5, pady=5)
 
 # SPELL CHECK BUTTON
@@ -161,6 +187,7 @@ button_spellcheck = Button(root,
                            bg="#e0e0e0",
                            fg="#46474f",
                            command=spellcheck_input)
+button_spellcheck_tt = Hovertip(button_spellcheck, "Spell Check")
 button_spellcheck.grid(row=4, columnspan=2, padx=5, pady=5)
 
 # STT BUTTON
@@ -169,7 +196,8 @@ button_stt = Button(root,
                     image=stt_photo,
                     bg="#e0e0e0",
                     fg="#46474f",
-                    command=speech_to_text)
+                    command=lambda: Thread(target=speech_to_text).start())
+button_stt_tt = Hovertip(button_stt, "Voice Recognition")
 button_stt.grid(row=4, column=1, padx=5, pady=5)
 
 # CLEAR BUTTON
@@ -185,7 +213,8 @@ button_tts2 = Button(root,
                      image=tts_photo,
                      bg="#e0e0e0",
                      fg="#46474f",
-                     command=text_to_speech_output)
+                     command=lambda: Thread(target=text_to_speech_output).start())
+button_tts2_tt = Hovertip(button_tts2, "Text-To-Speech")
 button_tts2.grid(row=4, column=4, padx=5, pady=5)
 
 # SWITCH BUTTON
@@ -195,12 +224,13 @@ switch_butt = Button(root,
                      bg="#e0e0e0",
                      fg="#46474f",
                      command=switcher)
+switch_butt_tt = Hovertip(switch_butt, "Switch Languages")
 switch_butt.grid(row=3, column=3, pady=5, padx=5)
 
 
 # COMBO BOXES
 # SOURCE COMBO BOX
-combo_1 = AutocompleteCombobox(root, width=30, completevalues=lg.lang)  # shows the languages in dropdown
+combo_1 = AutocompleteCombobox(root, width=30, completevalues=lg.lang,)  # shows the languages in dropdown
 combo_1.current(3)
 combo_1.grid(row=3, columnspan=3, pady=10, padx=10)
 
